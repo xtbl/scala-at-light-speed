63f8b3f3ee3e00fb9dfd85c5322bb1479c663ddf
Cristobal <correodecristobal@gmail.com>
2022-05-09 17:27:48 -0600

    object orientation

--- src/main/scala/com/rockthejvm/ObjectOrientation.scala
+++ src/main/scala/com/rockthejvm/ObjectOrientation.scala
@@ -8,7 +8,7 @@ object ObjectOrientation extends app {
 
   val anAnimal = new Animal
 
-  class Dog(val name: String) extends Animal 
+  class Dog(val name: String) extends Animal
   val aDog = new Dog("Lassie")
   aDog.name
 
@@ -32,7 +32,9 @@ object ObjectOrientation extends app {
   class Crocodile extends Animal with Carnivore with Philosopher {
     override def eat(animal: Animal): Unit = println("I'm eating you, animal!")
 
-    override def ?!(thought: String): Unit = println(s"I was thinking: $thought")
+    override def ?!(thought: String): Unit = println(
+      s"I was thinking: $thought"
+    )
   }
 
   aCroc = new Crocodile
@@ -42,8 +44,6 @@ object ObjectOrientation extends app {
 
   val basicMath = 1 + 2
   val anotherBasicMath = 1.+(2) // equivalent
-
-
 
   // singleton object
   object MySingleton {
@@ -56,15 +56,16 @@ object ObjectOrientation extends app {
   MySingleton.apply(65)
   MySingleton(65) // equivalent
 
-
-  object Animal { // companion - companion object 
+  object Animal { // companion - companion object
     // companions can access each other fields/methods
     // singleton Animal and instances are different things
     val canLiveIndefinetely = false
   }
 
-  val animalsCanLiveForever = Animal.canLiveIndefinetely // "static" fields/methods
+  val animalsCanLiveForever =
+    Animal.canLiveIndefinetely // "static" fields/methods
 
+  // this is another stash
 
   // case classes - lightweight datastructures with some boilerplate
   // - sensible equals and hash code
@@ -72,6 +73,33 @@ object ObjectOrientation extends app {
   // - companion with apply
   case class Person(name: String, age: Int)
   // may be constructed without new
-  val bob = Person("Bob", 54) // Person.apply("Bob", 54)
-  
+  val bob = Person("Bob", 54) // equivalente to Person.apply("Bob", 54)
+
+  // exceptions
+  try {
+    val x: String = null
+    x.length
+  } catch {
+    case e: Exception => "error message"
+  } finally {}
+
+  // generics
+  abstract class MyList[T] {
+    def head: T
+    def tail: MyList[T]
+  }
+
+  val aList: List[Int] = List(1, 2, 3) // List.apply(1,2,3)
+  val first = aList.head
+  val tail = aList.tail
+  val aStringList = List("hello", "scala")
+  val firstString = aList.head
+  // Point #1 we operate with IMMUTABLE values/objects
+  // modifications should return another object, don't mutate
+  // pros:
+  // better for multithreaded
+  // helps reasoning about the code
+  // Point #2 scala is closest to the object oriented ideal
+  // all values and methods are part of a class or object
+
 }
